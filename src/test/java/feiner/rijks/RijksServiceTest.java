2710a9290b1316ef181a145e6fadd74c74d88a25
leahfeiner <lfeiner02@gmail.com>
2024-05-15 21:14:32 -0400

    fixed indentation

--- src/test/java/feiner/rijks/RijksServiceTest.java
+++ src/test/java/feiner/rijks/RijksServiceTest.java
@@ -20,6 +20,33 @@ public class RijksServiceTest {
         ArtObjectCollection artObjectCollection = service.pageSearch(keyString, page).blockingGet();
         // then
         assertNotNull(artObjectCollection.artObjects);
+        ArtObjects artObject = artObjectCollection.artObjects[0];
+        assertNotNull(artObject);
+        assertNotNull(artObject.title);
+        assertNotNull(artObject.longTitle);
+        assertNotNull(artObject.principalOrFirstMaker);
+        assertNotNull(artObject.webImage);
+        WebImage webImage = artObject.webImage;
+        assertNotNull(webImage.url);
+        assertNotEquals(-1, webImage.offsetPercentageX);
+        assertNotNull(webImage.guid);
+        assertNotEquals(-1, webImage.width);
+        assertNotEquals(-1, webImage.height);
+        assertNotEquals(-1, webImage.offsetPercentageY);
+    }
+
+        @Test
+        void querySearch() {
+        // given
+        ApiKey apiKey = new ApiKey();
+        String keyString = apiKey.get();
+        RijksService service = new RijksServiceFactory().getService();
+        String query = "a";
+        int page = 0;
+        // when
+        ArtObjectCollection artObjectCollection = service.querySearch(keyString, page, query).blockingGet();
+        // then
+        assertNotNull(artObjectCollection.artObjects);
         ArtObjects artObjects = artObjectCollection.artObjects[0];
         assertNotNull(artObjects);
         assertNotNull(artObjects.title);
@@ -34,33 +61,6 @@ public class RijksServiceTest {
         assertNotEquals(-1, webImage.height);
         assertNotEquals(-1, webImage.offsetPercentageY);
     }
-
-    @Test
-    void querySearch() {
-    // given
-    ApiKey apiKey = new ApiKey();
-    String keyString = apiKey.get();
-    RijksService service = new RijksServiceFactory().getService();
-    String query = "a";
-    int page = 0;
-    // when
-    ArtObjectCollection artObjectCollection = service.querySearch(keyString, page, query).blockingGet();
-    // then
-    assertNotNull(artObjectCollection.artObjects);
-    ArtObjects artObjects = artObjectCollection.artObjects[0];
-    assertNotNull(artObjects);
-    assertNotNull(artObjects.title);
-    assertNotNull(artObjects.longTitle);
-    assertNotNull(artObjects.principalOrFirstMaker);
-    assertNotNull(artObjects.webImage);
-    WebImage webImage = artObjects.webImage;
-    assertNotNull(webImage.url);
-    assertNotEquals(-1, webImage.offsetPercentageX);
-    assertNotNull(webImage.guid);
-    assertNotEquals(-1, webImage.width);
-    assertNotEquals(-1, webImage.height);
-    assertNotEquals(-1, webImage.offsetPercentageY);
-}
 
     @Test
     void artistSearch() {
